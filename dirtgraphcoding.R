cd2d6a74ccc9a6f3607b3030253cbd1ed0528026
bghersi@tulane.edu <bghersi@tulane.edu>
2018-10-13 15:46:11 -0400

    Bruno's version

---
+++ dirtgraphcoding.R
@@ -0,0 +1,15 @@
+download.file(url="https://github.com/EEB504/EEB504/raw/master/GitR/dirtivore.rda",destfile = "dirtivore.rda")
+load("dirtivore.rda")
+str(dirtivore_df)
+
+non_numeric_indices<-which(is.na(as.numeric(dirtivore_df$rMax)))
+non_numeric_indices
+
+non_numeric_2<-which(is.na(as.numeric(dirtivore_df$MassKG)))
+non_numeric_2
+
+dirtivore_df$rMax<-as.numeric(dirtivore_df$rMax)
+dirtivore_df$MassKG<-as.numeric(dirtivore_df$MassKG)
+
+plot(log(dirtivore_df$MassKG),log(dirtivore_df$rMax))
+abline(lm(log(dirtivore_df$rMax)~log(dirtivore_df$MassKG)))
